be8ddd95af5cffdb0812a0744117ea400d0f34c3
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-12-22 19:27:03 -0600

    Completed setup of server.js file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require ("express");
+const path = require ("path");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
