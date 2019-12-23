a9768597447e735c3fb3b5d9b2a84926f19e1316
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-12-22 19:52:55 -0600

    added an array of friends for the finder to use

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+const friends = [
+{
+    "name": "Rock Lee",
+    "photo": "https://vignette.wikia.nocookie.net/naruto/images/9/97/Rock_Lee_Part_I.png/revision/latest?cb=20181229065526",
+    "scores": [3,2,4,4,5,2,1,3,4,4]
+},
+{
+    "name": "Me!Me!Me!",
+    "photo":"https://www.anime-planet.com/images/anime/covers/me-me-me-6827.jpg?t=1467850654",
+    "scores":[1,2,1,3,5,1,2,4,1,5]
+},
+{
+    "name": "Senzawa",
+    "photo": "https://i.redd.it/uenv8qn5og421.jpg",
+    "scores": [3,2,5,3,1,1,4,5,2,3]
+},
+{
+    "name": "Kugo",
+    "photo": "https://www.famousbirthdays.com/headshots/kugo-the-mighty-3.jpg",
+    "scores": [2,1,3,5,4,2,2,3,5,1]
+},
+]
+
+module.export = friends;
